d1b33a026457bdcd3fda6d9a488517869e1c6d7d
NazarovAn <naatonchrome@gmail.com>
2020-12-02 19:44:23 +0400

    third task done

--- src/components/HW-5.3/Collapse.js
+++ src/components/HW-5.3/Collapse.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Collapse.css'
 
+const expandedLable = 'Скрыть';
+const expandedArrow = '\u02C4';
+const collapsedLabel = 'Подробнее';
+const collapsedArrow = '\u02C5';
+const paragraph = (
+  <p>
+    'Альфа-Банк, основанный в 1990 году, является универсальным банком,
+    осуществляющим все основные виды банковских операций, представленных
+    на рынке финансовых услуг, включая обслуживание частных и корпоративных
+    клиентов, инвестиционный банковский бизнес, торговое финансирование и т.д.'
+  </p>
+);
+  
+
 export default function Collapse() {
+  const [expanded, setExpanded] = useState(true);
+
+  const handleCollapse = (event) => {
+    event.preventDefault();
+    setExpanded((collapsed) => collapsed ? false : true );
+  };
+
   return (
-    <div className="collapse">
-      Collapse
+    <div className="collapse__task">
+      { expanded ? paragraph : null }
+      <span className="collapse__task_btn" onClick={ handleCollapse }>
+        { expanded ? expandedLable : collapsedLabel }
+      </span>
+      &nbsp;
+      <span>{ expanded ? expandedArrow : collapsedArrow }</span>
     </div>
   )
 }
